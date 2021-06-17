372a8600e660017b4e56144acb53b8a1190fc7d6
sHva <cva.working@gmail.com>
2021-06-17 12:57:09 +0530

    #router and component based architecture

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const app = express();
-app.get('/req',(req,res)=>{
-    return res.send({"message":"re run the project code"});
-})
 
+{
+    app.get('/req',(req,res)=>{
+        return res.send({"message":"re run the project code"});
+    })
+}
 app.listen(1000,()=>console.log("Server running"))
---
+++ src/schema/REAEME.md
@@ -0,0 +1,12 @@
+//? Mongoose data types
+
+String
+Number
+Date
+Buffer
+Boolean
+Mixed
+ObjectId
+Array
+Decimal128
+Map
