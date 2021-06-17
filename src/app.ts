b863c7f4837eaccf09ac6052d1ac2273001596d6
sHva <cva.working@gmail.com>
2021-06-17 12:58:29 +0530

    #wow well formatted eslint

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 const app = express();
 
 {
-    app.get('/req',(req,res)=>{
+    app.get("/req",(req,res)=>{
         return res.send({"message":"re run the project code"});
-    })
+    });
 }
-app.listen(1000,()=>console.log("Server running"))
+app.listen(1000,()=>console.log("Server running"));
